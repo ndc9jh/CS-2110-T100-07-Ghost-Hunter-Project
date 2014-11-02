8905c30dfaabe8d41d21f87cc6ccf15f7d23987c
an5qn <an5qn@virginia.edu>
2014-11-02 17:38:18 -0500

    Sync Test

--- GhostHunter/src/edu/virginia/cs2110/MainActivity.java
+++ GhostHunter/src/edu/virginia/cs2110/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+// Test for sync ***
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
